17dd843e437c8b9716c0213be33deb3eef35b0c9
teplostanski <teplostanski@yandex.ru>
2023-09-15 22:59:29 +0300

    fix: i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,39 @@
 import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
+//import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
+import en from '../public/locales/en/translation.json'
+import ru from '../public/locales/ru/translation.json'
+
+const resources = {
+  en: {
+    // Namspaces
+    common: en,
+  },
+  ru: {
+    // Namspaces
+    common: ru,
+  },
+}
+
 i18n
-  .use(Backend)
+  //.use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'ru',
     debug: true,
-    detection: {
-      order: ['queryString', 'cookie'],
-      //cache: ['cookie'],
-    },
+    //detection: {
+    //  //order: ['queryString', 'cookie'],
+    //  //cache: ['cookie'],
+    //},
+    resources,
+
+    // Set default namespace
+    defaultNS: 'common',
+
+    lng: 'ru',
     interpolation: {
       escapeValue: false,
     },
