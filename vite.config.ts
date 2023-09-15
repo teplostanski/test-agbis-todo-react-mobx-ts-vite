f2564439d7279acce10a80232bed8ff1e987c6ea
teplostanski <teplostanski@yandex.ru>
2023-09-15 23:03:55 +0300

    fix: styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 //const generateScopedName = '[name]__[local]___[hash:base64:5]'
-import crypto from 'crypto';
+//import crypto from 'crypto';
 
 
 // https://vitejs.dev/config/
@@ -9,26 +9,26 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  css: {
-    modules: {
-      //localsConvention: 'camelCase',
-      generateScopedName: (name, filename, css) => {
-        const componentName = filename
-          .replace(/\.\w+$/, '')
-          .replace(/\.module+$/, '')
-          .split('/')
-          .pop(); 
+  //css: {
+  //  modules: {
+  //    //localsConvention: 'camelCase',
+  //    generateScopedName: (name, filename, css) => {
+  //      const componentName = filename
+  //        .replace(/\.\w+$/, '')
+  //        .replace(/\.module+$/, '')
+  //        .split('/')
+  //        .pop(); 
 
-        // Generate hash
-        const hash = crypto
-          .createHash('md5')
-          .update(css)
-          .digest('base64')
-          .substring(0, 5); 
+  //      // Generate hash
+  //      const hash = crypto
+  //        .createHash('md5')
+  //        .update(css)
+  //        .digest('base64')
+  //        .substring(0, 5); 
 
-        return `${componentName}__${name}__${hash}`;
-      },
-    },
-  },
+  //      return `${componentName}__${name}__${hash}`;
+  //    },
+  //  },
+  //},
   base: './',
 })
